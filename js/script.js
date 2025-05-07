e66e0b2a9b8d325f054450500c9c1a556140a38d
Leandrojr17 <leandronepomuceno1996@gmail.com>
2025-05-07 11:58:44 -0300

    Jogo Completo

--- js/script.js
+++ js/script.js
@@ -13,14 +13,20 @@ const jump = () => {
 
 }
 
-const loop = setInterval (() => {
+const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
 
-    if(pipePosition){
 
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
     }
-
-})
+    
+},10);
 
 document.addEventListener('keydown', jump);
